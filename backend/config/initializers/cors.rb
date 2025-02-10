10384c53cfcce33d71a8ec45efbf7f864c5ff306
kana miyazaki <>
2025-02-10 22:36:27 +0900

    fix: error

--- backend/config/initializers/cors.rb
+++ backend/config/initializers/cors.rb
@@ -1,7 +1,7 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   # 許可するオリジン（アクセス元）を指定
   allow do
-    origins 'https://task-runner-one.vercel.app/', 'http://localhost:4000'
+    origins 'https://task-runner-one.vercel.app', 'http://localhost:4000'
 
     # CORSの設定で、リソース（APIエンドポイント）の共有を許可
     resource '*', 
