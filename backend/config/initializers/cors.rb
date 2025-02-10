b8e4a4102e93ad68bc90b5a422023cfaa0fb8c14
kana miyazaki <>
2025-02-10 22:27:25 +0900

    fix: front url

--- backend/config/initializers/cors.rb
+++ backend/config/initializers/cors.rb
@@ -1,8 +1,7 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   # 許可するオリジン（アクセス元）を指定
-  # ここでは、`task-runner.vercel.app`（本番環境）と `localhost:4000`（開発環境）からのリクエストを許可
   allow do
-    origins 'https://task-runner.vercel.app', 'http://localhost:4000'
+    origins 'https://task-runner-one.vercel.app/', 'http://localhost:4000'
 
     # CORSの設定で、リソース（APIエンドポイント）の共有を許可
     resource '*', 
